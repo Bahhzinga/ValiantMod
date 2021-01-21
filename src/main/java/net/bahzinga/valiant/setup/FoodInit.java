f618c4f284aa2406545ca89b6c31067706cb5f9a
Bahhzinga <jackcarter.contact@gmail.com>
2021-01-21 02:08:49 +0000

    Added Sushi :3

--- src/main/java/net/bahzinga/valiant/setup/FoodInit.java
+++ src/main/java/net/bahzinga/valiant/setup/FoodInit.java
@@ -10,4 +10,10 @@ public class FoodInit {
             .effect(() -> new EffectInstance(Effects.SPEED, 100, 2), 0.5f)
             .effect(() -> new EffectInstance(Effects.POISON, 60), 0.05f).fastToEat().hunger(2).saturation(0.2f)
             .setAlwaysEdible().build();
+
+    public static final Food SUSHI = new Food.Builder()
+            .effect(() -> new EffectInstance(Effects.SPEED, 100, 2), 0.5f)
+            .effect(() -> new EffectInstance(Effects.INSTANT_HEALTH, 60), 0.05f).fastToEat().hunger(2).saturation(0.2f)
+            .setAlwaysEdible().build();
+
 }
--- src/main/java/net/bahzinga/valiant/setup/customitems/shovels/SapphireShovel.java
+++ src/main/java/net/bahzinga/valiant/setup/customitems/shovels/SapphireShovel.java
@@ -1,4 +1,46 @@
 package net.bahzinga.valiant.setup.customitems.shovels;
 
-public class SapphireShovel {
+import net.bahzinga.valiant.setup.ModItems;
+import net.minecraft.item.*;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.common.ToolType;
+
+public class SapphireShovel extends ShovelItem {
+
+    public static IItemTier shovel = new IItemTier() {
+        @Override
+        public int getMaxUses() {
+            return 500;
+        }
+
+        @Override
+        public float getEfficiency() {
+            return 4.5F;
+        }
+
+        @Override
+        public float getAttackDamage() {
+            return -492F;
+        }
+
+        @Override
+        public int getHarvestLevel() {
+            return 3; // 0: wood, 1: stone/gold, 2: iron, 3: diamond
+        }
+
+        @Override
+        public int getEnchantability() {
+            return 25;
+        }
+
+        @Override
+        public Ingredient getRepairMaterial() {
+            return Ingredient.fromItems(ModItems.SAPPHIRE.get());
+        }
+    };
+
+    public SapphireShovel() {
+        super(shovel, shovel.getMaxUses(), -1, new Item.Properties().group(ItemGroup.TOOLS).maxStackSize(1).addToolType(ToolType.SHOVEL, shovel.getHarvestLevel()).rarity(Rarity.RARE));
+    }
+
 }
